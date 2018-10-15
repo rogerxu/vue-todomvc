6b60a0cde8a42fdec9c1194fc1132e9b0bbc458d
Roger Xu <roger.xu.2007@gmail.com>
2018-10-16 00:52:38 +0800

    Add input box

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,6 +3,11 @@
     <!-- header -->
     <header class="header">
       <h1>TODO</h1>
+      <input type="text" class="new-todo"
+        autofocus
+        autocomplete="off"
+        placeholder="What needs to be done?"
+        @keyup.enter="addTodo">
     </header>
   </section>
 </template>
@@ -16,6 +21,19 @@ export default {
   computed: {
     todos() {
       return [];
+    },
+  },
+  methods: {
+    addTodo(e) {
+      const text = e.target.value;
+
+      if (text.trim()) {
+        // this.$store.dispatch('addTodo', text);
+        console.log('add item', text);
+      }
+
+      // clear text box
+      e.target.value = '';
     },
   },
 };
