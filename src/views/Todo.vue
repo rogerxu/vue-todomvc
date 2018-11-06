43cc069c849b58977c206724901806988e9d3af5
Roger Xu <roger.xu.2007@gmail.com>
2018-11-06 12:42:46 +0800

    Fix placeholder pseudo element

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -31,7 +31,7 @@
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2),
     0 25px 50px 0 rgba(0, 0, 0, 0.1);
 
-  input::input-placeholder {
+  input::placeholder {
     font-style: italic;
     color: #e6e6e6;
   }
